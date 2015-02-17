bc7a7597a49e5050b2510eceac1c311250cdfb5c
Fedil Grogan <fedil@ukneeq.com>
2015-02-17 00:47:45 -0600

    Create interpolate-filter.js
    Initial commit for interpolate-filter.js

---
+++ src/app/components/version/interpolate-filter.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.version.interpolate-filter', [])
+    .filter('interpolate', ['version', function(version) {
+        return function(text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        };
+    }]);
+})();
