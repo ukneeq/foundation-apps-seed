c1c6317d7c0674a0ef37dcc094342551d11bff8e
Fedil Grogan <fedil@ukneeq.com>
2015-02-17 00:49:04 -0600

    Create version-directive.js
    Initial commit for version-directive.js

---
+++ src/app/components/version/version-directive.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.version.version-directive', [])
+
+    .directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+            elm.text(version);
+        };
+    }]);
+    
+})();
